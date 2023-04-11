202d826b8b0155596944420dc36055d20abbe1c2
Jovan <jovanlemuel9103@gmail.com>
2023-04-11 09:10:38 +0700

    Initial Commit

---
+++ 0706012110048-JovanLouisLemuel-AFL3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  0706012110048-JovanLouisLemuel-AFL3
+//
+//  Created by MacBook Pro on 11/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 0706012110048-JovanLouisLemuel-AFL3/_706012110048_JovanLouisLemuel_AFL3App.swift
@@ -0,0 +1,17 @@
+//
+//  _706012110048_JovanLouisLemuel_AFL3App.swift
+//  0706012110048-JovanLouisLemuel-AFL3
+//
+//  Created by MacBook Pro on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _706012110048_JovanLouisLemuel_AFL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

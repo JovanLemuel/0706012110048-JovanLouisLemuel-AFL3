1798921b231012bc8a641b3dc0eb436111ccc633
Jovan <jovanlemuel9103@gmail.com>
2023-04-14 01:33:16 +0700

    section 3

--- 0706012110048-JovanLouisLemuel-AFL3/ContentView.swift
+++ 0706012110048-JovanLouisLemuel-AFL3/ContentView.swift
@@ -9,14 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Turtle Rock")
-                .font(.title)
-                
-        }
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                VStack(alignment: .leading) {
+                    Text("Turtle Rock")
+                        .font(.title)
+                    HStack {
+                        Text("Joshua Tree National Park")
+                            .font(.subheadline)
+                        Spacer()
+                        Text("California")
+                            .font(.subheadline)
+                    }
+                }
+                .padding()
+            }
     }
 }
 

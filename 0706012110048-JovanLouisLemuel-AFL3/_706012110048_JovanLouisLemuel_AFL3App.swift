05f2d78d3028ffa14b861c00c983c9e8cccb16b1
Jovan <jovanlemuel9103@gmail.com>
2023-04-15 01:41:03 +0700

    section 5

--- 0706012110048-JovanLouisLemuel-AFL3/_706012110048_JovanLouisLemuel_AFL3App.swift
+++ 0706012110048-JovanLouisLemuel-AFL3/_706012110048_JovanLouisLemuel_AFL3App.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct _706012110048_JovanLouisLemuel_AFL3App: App {
+    @StateObject private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }

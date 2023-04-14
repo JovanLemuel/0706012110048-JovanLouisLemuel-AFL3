9efae952b03bc359bb2044fe5044b68a3e68809d
Jovan <jovanlemuel9103@gmail.com>
2023-04-14 21:17:09 +0700

    Section 4

---
+++ 0706012110048-JovanLouisLemuel-AFL3/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  0706012110048-JovanLouisLemuel-AFL3
+//
+//  Created by MacBook Pro on 14/04/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+                   LandmarkRow(landmark: landmarks[0])
+                   LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}

d9bc9cdbcf2ee615514cd6b41e702e1a878ba5bb
Jovan <jovanlemuel9103@gmail.com>
2023-04-15 02:28:26 +0700

    section 3

---
+++ 0706012110048-JovanLouisLemuel-AFL3/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  0706012110048-JovanLouisLemuel-AFL3
+//
+//  Created by MacBook Pro on 15/04/23.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}

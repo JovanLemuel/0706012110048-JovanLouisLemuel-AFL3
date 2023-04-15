fce3471a6385113854264f97f55ac54eef211aa6
Jovan <jovanlemuel9103@gmail.com>
2023-04-15 20:36:34 +0700

    section 5 & 6

--- 0706012110048-JovanLouisLemuel-AFL3/Views/Categories/CategoryRow.swift
+++ 0706012110048-JovanLouisLemuel-AFL3/Views/Categories/CategoryRow.swift
@@ -10,18 +10,22 @@ import SwiftUI
 struct CategoryRow: View {
     var categoryName: String
     var items: [Landmark]
-
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text(categoryName)
                 .font(.headline)
                 .padding(.leading, 15)
                 .padding(.top, 5)
-
+            
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(alignment: .top, spacing: 0) {
                     ForEach(items) { landmark in
-                        CategoryItem(landmark: landmark)
+                        NavigationLink {
+                            LandmarkDetail(landmark: landmark)
+                        } label: {
+                            CategoryItem(landmark: landmark)
+                        }
                     }
                 }
             }
@@ -32,7 +36,7 @@ struct CategoryRow: View {
 
 struct CategoryRow_Previews: PreviewProvider {
     static var landmarks = ModelData().landmarks
-
+    
     static var previews: some View {
         CategoryRow(
             categoryName: landmarks[0].category.rawValue,

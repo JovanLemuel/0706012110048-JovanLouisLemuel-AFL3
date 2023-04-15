8280936ffeaade1ed5dd5d459270cb92f231f1e8
Jovan <jovanlemuel9103@gmail.com>
2023-04-15 20:17:26 +0700

    section 3

---
+++ 0706012110048-JovanLouisLemuel-AFL3/Views/Categories/CategoryItem.swift
@@ -0,0 +1,31 @@
+//
+//  CategoryItem.swift
+//  0706012110048-JovanLouisLemuel-AFL3
+//
+//  Created by MacBook Pro on 15/04/23.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem(landmark: ModelData().landmarks[0])
+    }
+}
+
---
+++ 0706012110048-JovanLouisLemuel-AFL3/Views/Categories/CategoryRow.swift
@@ -0,0 +1,42 @@
+//
+//  CategoryRow.swift
+//  0706012110048-JovanLouisLemuel-AFL3
+//
+//  Created by MacBook Pro on 15/04/23.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        CategoryItem(landmark: landmark)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+
+    static var previews: some View {
+        CategoryRow(
+            categoryName: landmarks[0].category.rawValue,
+            items: Array(landmarks.prefix(4))
+        )
+    }
+}

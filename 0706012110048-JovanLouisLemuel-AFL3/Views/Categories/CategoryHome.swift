dcd70a03a706d8ebac29bc3749e28b498760f61e
Jovan <jovanlemuel9103@gmail.com>
2023-04-15 19:36:27 +0700

    chapter 5 done section 1

---
+++ 0706012110048-JovanLouisLemuel-AFL3/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  0706012110048-JovanLouisLemuel-AFL3
+//
+//  Created by MacBook Pro on 15/04/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}

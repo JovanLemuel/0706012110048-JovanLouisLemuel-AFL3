c868e60d585d4e65fd9e4e809f3bdb7854103d42
Jovan <jovanlemuel9103@gmail.com>
2023-04-16 13:12:55 +0700

    chapter 6 done section 1

---
+++ 0706012110048-JovanLouisLemuel-AFL3/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  0706012110048-JovanLouisLemuel-AFL3
+//
+//  Created by MacBook Pro on 16/04/23.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}

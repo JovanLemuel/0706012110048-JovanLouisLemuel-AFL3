0d012d029013df6d0fab57e8e0604894b6e8e2a2
Jovan <jovanlemuel9103@gmail.com>
2023-04-27 16:37:39 +0700

    fix

---
+++ 0706012110048-JovanLouisLemuel-AFL3/Views/BadgeBackground.swift
@@ -0,0 +1,20 @@
+//
+//  BadgeBackground.swift
+//  0706012110048-JovanLouisLemuel-AFL3
+//
+//  Created by MacBook Pro on 15/04/23.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
--- 0706012110048-JovanLouisLemuel-AFL3/Views/ContentView.swift
+++ 0706012110048-JovanLouisLemuel-AFL3/Views/ContentView.swift
@@ -8,43 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+
+    enum Tab {
+        case featured
+        case list
+    }
+
     var body: some View {
-                VStack{
-                    MapView()
-                        .ignoresSafeArea(edges: .top)
-                        .frame(height: 300)
-                    
-                    CircleImage()
-                        .offset(y: -130)
-                        .padding(.bottom, -130)
-                    
-                    VStack(alignment: .leading) {
-                        Text("Turtle Rock")
-                            .font(.title)
-                        HStack {
-                            Text("Joshua Tree National Park")
-                            Spacer()
-                            Text("California")
-                        }
-                        .font(.subheadline)
-                        .foregroundColor(.secondary)
-                        
-                        Divider()
-                        
-                        Text("About Turtle Rock")
-                            .font(.title2)
-                        Text("Descriptive text goes here.")
-                    }
-                    .padding()
-                    
-                    Spacer()
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
                 }
-            
+                .tag(Tab.featured)
+
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }

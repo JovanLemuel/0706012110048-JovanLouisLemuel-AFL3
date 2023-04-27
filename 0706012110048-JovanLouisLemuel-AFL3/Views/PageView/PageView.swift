23f5a7152cf5570a50aafbb4d52cb6bf76a28ade
Jovan <jovanlemuel9103@gmail.com>
2023-04-27 14:20:54 +0700

    chapter 7 done section 1

---
+++ 0706012110048-JovanLouisLemuel-AFL3/Views/PageView/PageView.swift
@@ -0,0 +1,23 @@
+//
+//  PageView.swift
+//  0706012110048-JovanLouisLemuel-AFL3
+//
+//  Created by MacBook Pro on 27/04/23.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    
+    var body: some View {
+        PageViewController(pages: pages)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
---
+++ 0706012110048-JovanLouisLemuel-AFL3/Views/PageView/PageViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PageViewController.swift
+//  0706012110048-JovanLouisLemuel-AFL3
+//
+//  Created by MacBook Pro on 27/04/23.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+        
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+    }
+}
